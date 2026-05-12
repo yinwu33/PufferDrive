072bdd498fc607dd739f675ca977fecfe2f430fd
Wu, Yin 'PhD candidate' (T3-GC) <yin.wu@cariad.technology>
2026-05-12 09:10:24 +0200

    prepare to fetch

--- pufferlib/ocean/drive/drive.h
+++ pufferlib/ocean/drive/drive.h
@@ -2150,10 +2150,8 @@ void c_step(Drive *env) {
         // Reward agent if it is within X meters of goal and speed is below threshold
         bool within_distance = distance_to_goal < env->goal_radius;
         bool within_speed = current_speed <= env->goal_speed;
-        bool collision_free_before_goal = !env->entities[agent_idx].collided_before_goal;
 
-        if (within_distance && within_speed && collision_free_before_goal &&
-            !env->entities[agent_idx].current_goal_reached) {
+        if (within_distance && within_speed && !env->entities[agent_idx].current_goal_reached) {
             if (env->goal_behavior == GOAL_RESPAWN && env->entities[agent_idx].respawn_timestep != -1) {
                 env->rewards[i] += env->reward_goal_post_respawn;
                 env->logs[i].episode_return += env->reward_goal_post_respawn;
